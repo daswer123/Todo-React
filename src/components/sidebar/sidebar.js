f6758c1b29fd1f3cd7758cb25b68ad0cbeff9403
daswer123 <daswerq123@gmail.com>
2020-09-14 23:52:40 +0300

    Add copyright

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,8 +5,13 @@ const Sidebar = () => {
     return (
     <section className="sidebar-content sidebar-content-mobil" >
         <CategoryList/>
+        <div className="copyright">
+            <a className="copyright-link" href="https://github.com/daswer123/Todo-React">Ссылка на репозиторий</a>
+            <i style={{display:"block"}}>@Danil Boldyrev</i>
+        </div>
     </section>
     )
 }
 
 export default Sidebar;
+
